73f57959e72252b4029504d85c6992f946345b76
Marco Lewandowski <lewandowski@swms.de>
2020-08-05 15:18:19 +0200

    Added functionality to upload files from one day. Running the script from cloud_connection.py will run the function for yesterday. I will include this script to crontab to repeat it every morning.

--- cloud_connection.py
+++ cloud_connection.py
@@ -7,7 +7,7 @@ also see https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-upload
 
 @authors:   Arno Schiller (AS)
 @email:     schiller@swms.de
-@version:   v0.0.2
+@version:   v0.0.3
 @license:   ...
 
 VERSION HISTORY
@@ -15,11 +15,17 @@ Version:    (Author) Description:                                           Date
 v0.0.1      (AS) First initialize. Tested example code for S3 storage.      05.08.2020\n
 v0.0.2      (AS) First version to save data to the cloud into the           05.08.2020\n
                 test-dih4cps bucket.                                                  \n
+v0.0.2      (AS) Added functionality to upload every file created on the    05.08.2020\n
+                selected day. Starting this script will run the function              \n
+                for yesterday.                                                        \n
 
 ToDo:   - Add return value to the functions (bool)
 """
 import boto3
 import os
+import glob
+import datetime
+from datetime import timedelta
 
 class CloudConnection:
     """ 
@@ -33,12 +39,15 @@ class CloudConnection:
         secret key (password) of the cloud
     bucket_name : str
         used bucket to save the data to
+    cloud_url : str
+        url to cloud server
     recordsDir_name : str
         directory the video data is placed in
     """
     access_key = "minio"
     secret_key = "miniostorage"
     bucket_name = "test-dih4cps"
+    cloud_url = "https://minio.dih4cps.swms-cloud.com:9000/"
 
     recordsDir_name = "Recordings"
 
@@ -46,16 +55,12 @@ class CloudConnection:
         """
         Setup the S3 connection and the used bucket. Also define the recordings directory.
         """
-        self.s3 = boto3.resource('s3', 
+        # setup the S3 client
+        self. s3_client = boto3.client('s3', 
             aws_access_key_id = self.access_key,
             aws_secret_access_key = self.secret_key,
-            endpoint_url='https://minio.dih4cps.swms-cloud.com:9000/', 
+            endpoint_url=self.cloud_url, 
             config=boto3.session.Config(signature_version='s3v4'))
-
-        self.bucket = self.s3.Bucket(self.bucket_name)
-
-        for bucket in self.s3.buckets.all():
-            print(bucket.name)
 
         # get directory 
         parentDir_path = os.path.dirname(os.path.realpath(__file__))
@@ -75,14 +80,59 @@ class CloudConnection:
             object_name = file_name
 
         file_path = os.path.join(self.recordsDir_path, file_name)
+        self.s3_client.upload_file(file_path, self.bucket_name,object_name, Callback=ProgressPercentage(file_path))
 
-        data = open(file_path, 'rb')
-        self.bucket.put_object(Key=object_name, Body=data)
+    def uploadFilesFromDay(self, recording_name, day_datetime):
+        """ Upload every avi file that was created on that day.
 
+        Parameters
+        ----------
+        day_datetime : datetime 
+            day as datetime object to save the files from
+        recording_name : str
+            representative name for clear attribution of the recording (see WebcamRecorder)
+        """
+        # create the filter string
+        filter_str = "{0}_{1}-{2:02d}-{3:02d}_*-*-*.avi".format(recording_name, 
+                                    day_datetime.year,
+                                    day_datetime.month,
+                                    day_datetime.day)
+        filter_path = os.path.join(self.recordsDir_name, filter_str)
+        file_paths = glob.glob(filter_path)
 
-def sendFiles():
+        for path in file_paths:
+            file_name = os.path.basename(path)
+            self.uploadFileToCloud(file_name)
+
+import sys
+import threading
+class ProgressPercentage(object):
+    """ This class is used to show the progress percentage while uploading files.
+    Source: https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+    """
+    def __init__(self, filename):
+        self._filename = filename
+        self._size = float(os.path.getsize(filename))
+        self._seen_so_far = 0
+        self._lock = threading.Lock()
+        print("\n")
+
+    def __call__(self, bytes_amount):
+        # To simplify, assume this is hooked up to a single filename
+        with self._lock:
+            self._seen_so_far += bytes_amount
+            percentage = (self._seen_so_far / self._size) * 100
+            sys.stdout.write(
+                "\r%s  %s / %s  (%.2f%%)" % (
+                    os.path.basename(self._filename), self._seen_so_far, 
+                    self._size, percentage))
+            sys.stdout.flush()
+    
+         
+def sendFilesFromYesterday():
     s3Con = CloudConnection()
-    s3Con.uploadFileToCloud("output.avi")
+    yesterday = datetime.datetime.now() - timedelta(days = 1)
+    s3Con.uploadFilesFromDay("test1", yesterday)
 
 if __name__ == "__main__":
-    sendFiles()
+    sendFilesFromYesterday()
